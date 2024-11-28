8eaf9a47452efdadbad0efafa30a3d3383653445
SebeO <pekacki.sebastian1@gmail.com>
2024-11-28 20:08:11 +0100

    B1.1v

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import './App.css';
 function App() {
     return (
         <div className="Fullpage">
+            {/* Sekcja TOP */}
             <div id="TOP">
                 <div id="Menu">
                     {/* Miejsce na logo po lewej stronie */}
                     <div className="logo">
-                        <div className="logo">
-                            <a href="index.html">
-                                <img src="/logo.png" alt="Logo"/>
-                            </a>
-                        </div>
-
-
+                        <a href="index.html">
+                            <img src="/logo.png" alt="Logo" />
+                        </a>
                     </div>
 
                     {/* Menu po lewej stronie */}
@@ -31,6 +28,25 @@ function App() {
                         <button className="login-button">Zaloguj się</button>
                     </div>
                 </div>
+            </div>
+
+            {/* Sekcja MID */}
+            <div className="MID">
+                <div className="carousel">
+                    <div className="tile">Kafelek 1</div>
+                    <div className="tile">Kafelek 2</div>
+                    <div className="tile">Kafelek 3</div>
+                </div>
+            </div>
+
+            {/* Sekcja MID-2 */}
+            <div id="MID-2">
+                <p>Miejsce na dodatkową treść</p>
+            </div>
+
+            {/* Sekcja BOTTOM */}
+            <div id="BOTTOM">
+                <h2>STOPKA</h2>
             </div>
         </div>
     );
